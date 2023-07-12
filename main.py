0de6f9fc95943cc62b6cd3e60a71d82c85f33db9
Zenchang Sun <Supercharger.zc1@gmail.com>
2023-07-12 10:33:21 -0400

    form data stuff

--- main.py
+++ main.py
@@ -11,6 +11,25 @@ def home():
     """
     return render_template("home.html")
 
+@app.route("/new_entry", methods=["POST"])
+def new_entry():
+    """
+    This is when the user asks FALCON a new question, and enters it.
+    The question should be saved as a json file, displayed in the 
+    chat log, and then sent to the LLM.
+    """
+
+    if request.method == "POST":
+        print("It worked")
+        entry = request.form
+        print(f"Here is the entry: {entry}")
+
+        # TODO: Send info to LLM somehow
+
+    
+    return render_template("home.html")
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True) # Set debug = True for live changes in development
