7dd5b1a64fcbb34ba4dd5c5f19093049e2a4c3e0
Zenchang Sun <Supercharger.zc1@gmail.com>
2023-07-13 12:01:48 -0400

    hella nice space stuff

--- static/home.js
+++ static/home.js
@@ -12,6 +12,8 @@ var form_entry = document.getElementById("entry");
 var chat_box = document.getElementById("chat_log");
 
 function upload(){
+    // boolean flag
+    var enter_flag = new Boolean(false);
     const request = new XMLHttpRequest();
     var form_entry = document.getElementById("entry").value;
     request.open('POST', `/new_entry/${form_entry}`);
@@ -19,13 +21,33 @@ function upload(){
         // response is what the flask function returns
         const response = request.responseText;
         var div = document.createElement("div");
-        div.id = "user_chat";
-        const node = document.createTextNode(form_entry);
-        div.appendChild(node);
-        chat_box.appendChild(div);
+
+        // make sure div is not empty, takes care of the spaces
+        if (!/\S/.test(form_entry)) {
+            // Didn't find something other than a space which means it's empty
+            enter_flag = true;
+        }
+        else {
+            div.id = "user_chat";
+            const node = document.createTextNode(form_entry);
+            div.appendChild(node);
+            chat_box.appendChild(div);
+        }
+
+        
     }; 
-    request.send();
-    chat_box.scrollTop = chat_box.scrollTopMax;
+    // make sure div is not empty
+    
+    if (enter_flag == false) {
+        request.send();
+        // change scroll, it doesn't work
+        chat_box.scrollTop = chat_box.scrollTopMax;
+        document.getElementById("entry").value = "";
+    }
+        
+    
+    
+    
 }
 
 //check if button is manually hit, check this if we add another button
