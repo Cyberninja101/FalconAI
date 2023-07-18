acffe8ede5d4930878d10ddb0e43894bd059fa97
Kenneth W <klw.phoenix.333@gmail.com>
2023-07-18 16:53:52 -0400

    Prototype for context.

--- Web_App/dummy_model.py
+++ Web_App/dummy_model.py
@@ -1,5 +1,5 @@
 import os
-from langchain import PromptTemplate, HuggingFaceHub
+from langchain import PromptTemplate, HuggingFaceHub, FewShotPromptTemplate
 from langchain.chains import LLMChain, ConversationChain
 from langchain.chains.conversation.memory import (ConversationBufferMemory, 
                                                   ConversationSummaryMemory, 
@@ -10,7 +10,7 @@ class model():
         os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
 
         # build prompt template for simple question-answering
-        self.template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. 
+        self.template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from the Current conversation. 
             If the AI does not know the answer to a question, it truthfully says it does not know. 
 
             Current conversation:
@@ -20,10 +20,35 @@ class model():
             AI: """
 
         self.PROMPT = PromptTemplate(input_variables=["history", "input"], template=self.template)
+
+
+        # # now break our previous prompt into a prefix and suffix
+        # # the prefix is our instructions
+        # prefix = """The following are exerpts from conversations with an AI
+        # assistant. The assistant is typically sarcastic and witty, producing
+        # creative  and funny responses to the users questions. Here are some
+        # examples: 
+        # """
+        # # and the suffix our user input and output indicator
+        # suffix = """
+        # User: {query}
+        # AI: """
+
+        # # now create the few shot prompt template
+        # few_shot_prompt_template = FewShotPromptTemplate(
+        #     examples=examples,
+        #     example_prompt=self.template,
+        #     prefix=prefix,
+        #     suffix=suffix,
+        #     input_variables=["query"],
+        #     example_separator="\n\n"
+        # )
+
+        
         # initialize HF LLM
         self.flan_t5 = HuggingFaceHub(
             repo_id="google/flan-t5-xxl",
-            model_kwargs={"temperature":0.01}
+            model_kwargs={"temperature":0.1}
         )
 
         self.conversation = ConversationChain(
