2af4060b709aff097e47bf36115a024075befe82
Kenneth W <klw.phoenix.333@gmail.com>
2023-07-19 10:00:32 -0400

    Commit

--- Web_App/dummy_model.py
+++ Web_App/dummy_model.py
@@ -5,42 +5,47 @@ from langchain.chains.conversation.memory import (ConversationBufferMemory,
                                                   ConversationSummaryMemory, 
                                                   ConversationBufferWindowMemory,
                                                   ConversationKGMemory)
+from pdfReader import read_pdf
+
 class model():
     def __init__(self):
         os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
 
         # build prompt template for simple question-answering
-        self.template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from the Current conversation. 
-            If the AI does not know the answer to a question, it truthfully says it does not know. 
-
-            Current conversation:
-            {history}
-
+        self.template = """
             Human: {input}
-            AI: """
+            AI: {answer}"""
 
-        self.PROMPT = PromptTemplate(input_variables=["history", "input"], template=self.template)
+        self.PROMPT = PromptTemplate(input_variables=["input", "answer"], template=self.template)
 
 
-        # # now break our previous prompt into a prefix and suffix
-        # # the prefix is our instructions
-        # prefix = """The following are exerpts from conversations with an AI
-        # assistant. The assistant is typically sarcastic and witty, producing
-        # creative  and funny responses to the users questions. Here are some
-        # examples: 
+        # now break our previous prompt into a prefix and suffix
+        # the prefix is our instructions
+        # self.prefix = """
+        # The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from the Current conversation. 
+        # If the AI does not know the answer to a question, it truthfully says it does not know. 
+
+        # Current conversation:
+        # {history}
+
+        # The following is a document containing vital context and knowledge. 
+        # The AI will respond to new questions based on the content of the file. Here is the context:
         # """
         # # and the suffix our user input and output indicator
-        # suffix = """
-        # User: {query}
-        # AI: """
+        # self.suffix = """
+        # Document: {query}
+
+        # User: {input}
+        # Ai: 
+        # """
 
         # # now create the few shot prompt template
-        # few_shot_prompt_template = FewShotPromptTemplate(
-        #     examples=examples,
-        #     example_prompt=self.template,
-        #     prefix=prefix,
-        #     suffix=suffix,
-        #     input_variables=["query"],
+        # self.few_shot_prompt_template = FewShotPromptTemplate(
+        #     examples=[{"Document": read_pdf("RadarInfo/RadarHandbook_CH001.pdf")}],
+        #     example_prompt=self.PROMPT,
+        #     prefix=self.prefix,
+        #     suffix=self.suffix,
+        #     input_variables=["history","query","input"],
         #     example_separator="\n\n"
         # )
 
@@ -58,3 +63,6 @@ class model():
         )
     def run(self, text):
         return([self.conversation(text)["response"], self.conversation.memory.buffer])
+
+chatbot = model()
+print(chatbot.run("How are you?"))
